51380b39452f3a41f2506b5793ca0c5a2d5f6e33
Ms-Angel <53004922+Ms-Angel@users.noreply.github.com>
2019-07-30 15:14:46 +0800

    Add files via upload

---
+++ sum1.php
@@ -0,0 +1,38 @@
+<?php
+function series_sum($n) {
+    $a=1/(1+3*$n);
+    $b=1+$a;
+    $ans=$b*($n+1)/2;
+    echo $ans."<br>";
+  }
+  series_sum(4);
+
+function series_sum1($n) {
+    if($n==0)
+    {
+        echo $n=0.00;
+    }else{
+    $ans=0;
+    for($x=0;$x<=$n;$x++)
+    {
+       $ans+=1/(1+(3*$x));
+    }
+    echo round($ans, 2);
+      echo "<br>";
+    }
+  }
+  series_sum2(100);
+  function series_sum2($n) {
+    if($n==0)
+    {
+        echo $n=0.00;
+    }else{
+    $ans=0;
+    for($x=1;$x<=3*$n-2;$x=$x+3)
+    {
+       $ans+=1/$x;
+    }
+    echo round($ans, 2);}
+  }
+  series_sum2(0);
+?>
